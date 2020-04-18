64e06354d863d87aef714eb1cd0c2c6ca7c7039d
Ewelina Szoda <ewelinaszoda@gmail.com>
2020-04-18 13:29:07 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,23 @@
+require 'pry'
+
+data = {
+  :color => {
+    :purple => ["Theo", "Peter Jr.", "Lucky"],
+    :grey => ["Theo", "Peter Jr.", "Ms. K"],
+    :white => ["Queenie", "Andrew", "Ms. K", "Alex"],
+    :brown => ["Queenie", "Alex"]
+  },
+  :gender => {
+    :male => ["Alex", "Theo", "Peter Jr.", "Andrew", "Lucky"],
+    :female => ["Queenie", "Ms. K"]
+  },
+  :lives => {
+    "Subway" => ["Theo", "Queenie"],
+    "Central Park" => ["Alex", "Ms. K", "Lucky"],
+    "Library" => ["Peter Jr."],
+    "City Hall" => ["Andrew"]
+  }
+}
 
 def nyc_pigeon_organizer(data)
   pigeon_list = {}
@@ -54,4 +74,26 @@ def nyc_pigeon_organizer(data)
   end
   pigeon_list
 end
-=end
+=end 
+
+def nyc_pigeon_organizer(data)
+  new_hash = {}
+  data.each do |key, value|
+    value.each do |new_value, names|
+      names.each do |name|
+        
+        if !new_hash[name]
+          binding.pry
+          new_hash[name] = {}
+        end
+
+        if !new_hash[name][key]
+          new_hash[name][key] = []
+        end
+        new_hash[name][key] << new_value.to_s
+
+      end
+    end
+  end
+  new_hash
+end
